7e31b70cbab7c01cc0000c2e647695a89ed260cc
Dian Prasetya <71544227+dadayan1234@users.noreply.github.com>
2024-04-27 13:54:44 +0000

    New Layout and End Point

--- main.py
+++ main.py
@@ -1,12 +1,46 @@
-from flask import Flask, request, jsonify, render_template
-from datetime import datetime
 import json
-from flask_cors import CORS
+from datetime import datetime
 from threading import Thread
-import time
+
+from flask import Flask, jsonify, render_template, request
+from flask_cors import CORS
+from replit import db
 
 app = Flask(__name__)
 CORS(app)
+
+######################## DATA HANDLING #########################
+@app.route("/save_data", methods=["POST"])
+def save_data():
+    req_data = request.get_json()
+    timestamp = req_data.get("timestamp")
+    current = req_data.get("current")
+    volt = req_data.get("volt")
+
+    if timestamp and current and volt:
+        db[timestamp] = {"current": current, "volt": volt}
+        return jsonify({"success": True, "message": "Data saved successfully"})
+    else:
+        return jsonify({"success": False, "message": "Invalid data format"})
+
+
+@app.route("/get_all_data", methods=["GET"])
+def get_all_data():
+    return jsonify(dict(db))
+
+@app.route("/get_data_by_timestamp", methods=["GET"])
+def get_data_by_timestamp():
+    timestamp = request.args.get("timestamp")
+    if not timestamp:
+        return jsonify({"success": False, "message": "Timestamp parameter is missing"})
+
+    data = db.get(timestamp)
+    if data:
+        return jsonify(data)
+    else:
+        return jsonify({"success": False, "message": "Data not found for the provided timestamp"})
+
+#################################################################
 
 # Data dummy
 data = []
@@ -19,6 +53,10 @@ def home():
 @app.route("/dashboard")
 def dashboard():
   return render_template("dashboard.html")
+
+@app.route("/dashboard_2")
+def dashboard_2():
+  return render_template("dashboard_2.html")
 
 @app.route("/dashboard_adv")
 def dashboard_adv():
@@ -59,3 +97,5 @@ def get_data_2():
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=80)  # Bind to all interfaces
+
+
