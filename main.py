2cc5805c293f3e3f8a3a29393cd161c4a4a1afd8
Dian Prasetya <71544227+dadayan1234@users.noreply.github.com>
2024-04-30 14:42:29 +0000

    APi cleansing

--- main.py
+++ main.py
@@ -113,15 +113,7 @@ def get_data_by_timestamp():
         "message": "Data not found for the provided timestamp"})
 
 #################################################################
-
-# Data dummy
-data = []
-
 @app.route("/")
-@app.route("/home")
-def home():
-  return render_template("home.html")
-  
 @app.route("/dashboard")
 def dashboard():
   return render_template("dashboard.html")
@@ -130,42 +122,7 @@ def dashboard():
 def dashboard_2():
   return render_template("dashboard_2.html")
 
-@app.route("/dashboard_adv")
-def dashboard_adv():
-  return render_template("advanced.html")
-  
-def post_data_thread(req_data):
-  if req_data:  # Check if data exists to prevent unnecessary writes
-      with open("data.json", "r") as f:
-          data = json.load(f)
-      req_data["timestamp"] = datetime.now().isoformat()
-      data["data"].append(req_data)  # Menambahkan data ke dalam "data" array
-      with open("data.json", "w") as f:
-          json.dump(data, f)
-      print(f"Data received and saved: {req_data}")
-
-@app.route("/post", methods=["POST"])
-def post_data():
-  req_data = request.get_json()
-  thread = Thread(target=post_data_thread, args=(req_data, ))
-  thread.start()
-  return jsonify({"success": True})
-
-@app.route("/get", methods=["GET"])
-def get_data():
-  with open("data.json", "r") as f:
-      data = json.load(f)
-  return jsonify(data)  # Mengembalikan seluruh data dari "data.json"
-
-@app.route("/get_data", methods=["GET"])
-def get_data_2():
-    with open("data.json", "r") as f:
-        data = json.load(f)
-
-    # Mengambil data terakhir dari data.json
-    latest_data = data["data"][-1] if data["data"] else {}
-
-    return jsonify(latest_data)
+ 
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=80)  # Bind to all interfaces
